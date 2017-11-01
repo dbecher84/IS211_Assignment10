e151cdf39eeddc2dfda67f9651ef97622a9272ec
dbecher84 <dbecher84@gmail.com>
2017-11-01 19:26:07 -0400

    Add files via upload

---
+++ music.sql
@@ -0,0 +1,7 @@
+CREATE TABLE artists (artist_id INTEGER, artist_name TEXT)
+
+CREATE TABLE albums (album_id INTERGER, album,name TEXT, artist_id INTEGER)
+
+CREATE TABLE songs (song_id, INTEGER, song_name TEXT, artist_id INTEGER,
+                    album_id, INTERGER, Track_num INTEGER, song_len INTEGER)
+
